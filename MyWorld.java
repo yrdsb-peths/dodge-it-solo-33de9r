440f8e7bbdfe56875a24366a1f74b91e18199d95
33de9r <chenjiayu2033@gmail.com>
2025-04-14 14:29:22 -0400

    Penguin and Bomb added.

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Penguin player = new Penguin();
+        addObject(player, 100, 100);
+        
+        Bomb one = new Bomb();
+        addObject(one, 500, 100);
     }
 }
